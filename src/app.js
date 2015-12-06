d9a8082b2c03b5e75f962a8c29ac3bf9aedbd043
Igor Czerwinski <igor.czerwinski@fundingcircle.com>
2015-12-06 15:41:37 +0000

    Refactor main function to follow MVI pattern.

--- src/app.js
+++ src/app.js
@@ -4,38 +4,54 @@ import {h, makeDOMDriver} from '@cycle/dom';
 
 import makeWebSocketDriver from './cycle-websocket-driver';
 
-function main({DOM, ws}) {
+function intent(DOM, ws) {
   const sendClicks$ = DOM.select('[data-js-send]')
     .events('click');
   const messageText$ = DOM.select('[data-js-text]')
     .events('input')
     .map(event => event.target.value);
 
+  return {
+    messageSent$: messageText$.sample(sendClicks$)
+    messageReceived$: ws
+  };
+}
+
+function view(state$) {
+  return $state.map(messages =>
+    h('div', [
+      h('input', {
+        type: 'text',
+        attributes: {
+          'data-js-text': ''
+        }
+      }, ['Send']),
+      h('button', {
+        attributes: {
+          'data-js-send': ''
+        }
+      }, ['Send']),
+      h('ol', [
+        messages.map(msg => h('li', msg))
+      ])
+    ])
+  );
+}
+
+function model(messageReceived$) {
+  return actions.messageReceived$
+    .scan((messages, msg) => {
+      messages.push(msg)
+      return messages;
+    }, []);
+}
+
+function main({DOM, ws}) {
+  const actions = intent(DOM, ws);
+
   const requests = {
-    ws: messageText$.sample(sendClicks$),
-    DOM: ws
-      .scan((messages, msg) => {
-        messages.push(msg)
-        return messages;
-      }, [])
-      .map(messages =>
-        h('div', [
-          h('input', {
-            type: 'text',
-            attributes: {
-              'data-js-text': ''
-            }
-          }, ['Send']),
-          h('button', {
-            attributes: {
-              'data-js-send': '' 
-            }
-          }, ['Send']),
-          h('ol', [
-            messages.map(msg => h('li', msg))
-          ])
-        ])
-      )
+    ws: actions.messageSent$
+    DOM: view(model(action.messageReceived$))
   };
 
   return requests;
